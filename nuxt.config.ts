cb7ddd4153fd57d703e91b679fd0260d84293abc
EliteEngineer0 <EliteEngineer0@hotmail.com>
2024-03-30 14:02:27 +0100

    ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
     // Will be available in both server and client
     tokenSecret: process.env.TOKEN_SECRET,
     tokenExpiration: process.env.TOKEN_EXPIRES,
-    tokenName: process.env.TOKEN_NAME
+    tokenName: process.env.TOKEN_NAME,
+    databaseUrl: process.env.DATABASE_URL
   },
 
   modules: ['@nuxtjs/tailwindcss','nuxt-icon'],
---
+++ pages/forum.vue
@@ -0,0 +1,9 @@
+<template >
+    <div class="text-dark text-center">
+      <div>
+        <h1 class="text-2xl">Forum</h1>
+        <p class="mt-4">Questo è un sito fantastico di scacchi.</p>
+      </div>
+    </div>
+</template>
+  
