5311e46860edd942022459561fa60674aaa14b93
EliteEngineer0 <EliteEngineer0@hotmail.com>
2024-03-30 17:01:23 +0100

    ...

--- server/db/index.ts
+++ server/db/index.ts
@@ -4,33 +4,33 @@ const prisma = new PrismaClient();
 
 export { prisma }
 
-// async function seed() {
-//   // Create posts
-//   const postsData = [
-//     {
-//       userId: 1, // Assuming admin user is at index 0
-//       content: "Che sito schifoso!",
-//     },
-//     {
-//       userId: 2, // Assuming user user is at index 1
-//       content: "Ciao a tutti!!!!!!!",
-//     }
-//     // Add more post data as needed
-//   ];
+async function seed() {
+  // Create posts
+  const postsData = [
+    {
+      userId: 1, // Assuming admin user is at index 0
+      content: "Che sito schifoso!",
+    },
+    {
+      userId: 2, // Assuming user user is at index 1
+      content: "Ciao a tutti!!!!!!!",
+    }
+    // Add more post data as needed
+  ];
 
-//   const createdPosts = await Promise.all(postsData.map(postData =>
-//     prisma.post.create({
-//       data: postData,
-//     })
-//   ));
-//   console.log("Posts created:", createdPosts);
-// }
+  const createdPosts = await Promise.all(postsData.map(postData =>
+    prisma.post.create({
+      data: postData,
+    })
+  ));
+  console.log("Posts created:", createdPosts);
+}
 
-// seed()
-//   .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
